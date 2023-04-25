bb30571f3da365c3036be330300d7ce4c2902149
Jay <1356573671@qq.com>
2023-04-25 21:25:22 +0800

    feat: 移除@路径别名的用法

--- vitest.config.js
+++ vitest.config.js
@@ -4,9 +4,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue() /* 引入vue扩展，用于识别vue文件 */],
   test: {
-    alias: {
-      "@": "src/",
-    },
     environment: "jsdom", // 全局引入DOM环境
   },
 });
