e159e5a38cb8bce0a0cac9406917c8481af2dd01
Jay <Jay040030@outlook.com>
2023-11-17 20:59:48 +0800

    chore: 去除打包是sourceMap文件的产出

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  productionSourceMap: false,
   transpileDependencies: true,
   css: {
     extract: false, // 强制css内联，以便于引入lib组件时不需要再引入对应的css
