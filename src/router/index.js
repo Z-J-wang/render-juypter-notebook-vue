54bdf9e45df9851bc89448202b349bb188df0c8d
Jay <Jay040030@outlook.com>
2024-09-26 13:59:25 +0800

    feat: Update router to use hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const routes = [{ path: '/', name: 'home', component: HomeView }];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 });
 
